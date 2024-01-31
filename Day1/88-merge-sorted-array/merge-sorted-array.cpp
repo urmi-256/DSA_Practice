9e69739eb8aec3f0be27c95422c481c80c5f74fd
Urmi Chauhan <urmi.chauhan05@gmail.com>
2024-01-31 23:06:41 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (13.81%) - LeetSync

--- Day1/88-merge-sorted-array/merge-sorted-array.cpp
+++ Day1/88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,44 +1,6 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-    /* my code
-       int i=0,j=0;
-       while(i<m && j<n){
-           if(nums1[i] > nums2[j]){
-               swap(nums1[i],nums2[j]);
-               //cout<<nums1[i]<<" "<<nums2[j]<<endl;
-           }
-           sort(nums2.begin(),nums2.end());
-           i++;
-       }
-       while(i<nums1.size() && j<n){
-           swap(nums1[i],nums2[j]);
-           //cout<<nums1[i]<<" "<<nums2[j]<<endl;
-           i++;
-           j++;
-       } 
-
-      // TC -  O(n * nlogn)   SC - (O(1))
-      */
-  
-      /*
-      int i=m-1,j=0;
-      while(i>=0&& j<n){
-       if(nums1[i] > nums2[j]){
-           swap(nums1[i],nums2[j]);
-       }
-       else break;
-       i--;j++;
-      }
-      sort(nums1.begin(),nums1.begin()+m);
-      sort(nums2.begin(),nums2.end());
-      j=0;
-      for(int i=m;i<nums1.size();i++){
-          nums1[i] = nums2[j++];
-      }
-      // TC - (Omlogm + nlogn + min(n,m))  SC - O(1)
-      */
-
       // gap method - shell sort
       // place left at start and right at left+gap move left and right and sort till right<n also reduce gap
       // and again start left=0,right=gap+left till gap is reduced to less than 1
