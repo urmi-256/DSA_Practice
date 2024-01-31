b5abafcfdb2bb9b03896772bd8afeb3fc8a9eb72
Urmi Chauhan <urmi.chauhan05@gmail.com>
2024-01-31 22:12:04 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (13.81%) - LeetSync

--- Day1/88-merge-sorted-array/merge-sorted-array.cpp
+++ Day1/88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    /* my code
        int i=0,j=0;
        while(i<m && j<n){
            if(nums1[i] > nums2[j]){
@@ -18,5 +19,22 @@ public:
        } 
 
       // TC -  O(n * nlogn)   SC - (O(1))
+      */
+
+      int i=m-1,j=0;
+      while(i>=0&& j<n){
+       if(nums1[i] > nums2[j]){
+           swap(nums1[i],nums2[j]);
+       }
+       else break;
+       i--;j++;
+      }
+      sort(nums1.begin(),nums1.begin()+m);
+      sort(nums2.begin(),nums2.end());
+      j=0;
+      for(int i=m;i<nums1.size();i++){
+          nums1[i] = nums2[j++];
+      }
+      // TC - (Omlogm + nlogn + min(n,m))  SC - O(1)
      }
 };
