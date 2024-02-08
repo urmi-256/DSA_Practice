8e18fbe3e30a425eb7b58e7155a5f4866026be82
Urmi Chauhan <urmi.chauhan05@gmail.com>
2024-02-08 23:48:01 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (34.51%) - LeetSync

--- Day2/74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ Day2/74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
-    bool binarySearch(int target,vector<vector<int>>matrix,int i){
-      int l=0,h = matrix[0].size()-1;
-      while(l<=h){
-          int mid = (l+h)/2;
-          if(matrix[i][mid] == target){
-              return true;
-          }
-          else if(matrix[i][mid] < target){
-             l = mid+1;
-          }
-          else{
-           h = mid-1;
-          }
-      }
-      return false;  
-    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-      // one approach - O(m * log m);
-      int m = matrix.size();  
-      int n = matrix[0].size();
-      for(int i=0;i<m;i++){
-         if(matrix[i][0] <= target&& matrix[i][n-1]>=target){
-            return binarySearch(target,matrix,i);
-         }
-      }
-      return false;
+        int m = matrix.size(),n = matrix[0].size();
+       int l = 0, h = (n*m-1);
+       while(l<=h){
+           int mid = (l+h)/2;
+           if(matrix[mid/n][mid%n] == target) return true;
+           else if(matrix[mid/n][mid%n] < target) l = mid+1;
+           else h = mid-1;
+       }
+       return false;
+        // TC - Log(nm) SC - (O(1)) 
     }
-   //TC - O(m + logn) SC- (O(1));  as we are doing binary search only for one row
 };
