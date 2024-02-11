48f3f4c8f7de46f688ca50a6db7e8f9cddd76c70
Urmi Chauhan <urmi.chauhan05@gmail.com>
2024-02-11 22:36:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (8.20%) - LeetSync

--- Day2/62-unique-paths/unique-paths.cpp
+++ Day2/62-unique-paths/unique-paths.cpp
@@ -14,15 +14,20 @@
 // };
 class Solution {
 public:
-    int findPath(int m,int n,int i,int j,vector<vector<int>>&dp){
-       if(i == m-1 && j == n-1) return 1;
-       if(i >= m || j>=n )return 0;
-       if(dp[i][j]!=-1) return dp[i][j];
-       return dp[i][j] = findPath(m,n,i+1,j,dp)+findPath(m,n,i,j+1,dp); 
-    }
     int uniquePaths(int m, int n) {
-    vector<vector<int>>dp(m,vector<int>(n,-1));
-    return findPath(m,n,0,0,dp);
+    vector<vector<int>>dp(m,vector<int>(n,0));
+    dp[m-1][n-1] = 1;
+    for(int i=m-1;i>=0;i--){
+        for(int j =n-1;j>=0;j--){
+          if(j+1>=n && i+1<m){
+              dp[i][j] += (dp[i+1][j]);
+          }
+          else if(i+1>=m && j+1<n) dp[i][j] += (dp[i][j+1]);
+          else if(i+1<m && j+1<n) dp[i][j] += (dp[i+1][j]+dp[i][j+1]);
+        }
+    }
+    return dp[0][0];
     }
     // TC - O(m*n) SC - (On*m)
+    // dp- memoisation
 };
